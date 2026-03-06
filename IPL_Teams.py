803f74194e25bff3d2a99e09117b99d99137ac4a
Sahilpatil09 <sahilpatil3743@gmail.com>
2026-03-06 13:21:11 +0530

    updated files

---
+++ IPL_Teams.py
@@ -0,0 +1,35 @@
+# IPL Teams and Players Data
+
+MI=[{"jn":45,"p_name":"Rohit Sharma","team":"mumbai indians","runs":5643,"wickets":20},
+    {"jn":18,"p_name":"Suryakumar Yadav","team":"mumbai indians","runs":3456,"wickets":10},
+    {"jn":33,"p_name":"Kieron Pollard","team":"mumbai indians","runs":4567,"wickets":35},
+    {"jn":99,"p_name":"Ishan Kishan","team":"mumbai indians","runs":2345,"wickets":5},
+    {"jn":33,"p_name":"Hardik Pandya","team":"mumbai indians","runs":3456,"wickets":45},
+    {"jn":93,"p_name":"Jasprit Bumrah","team":"mumbai indians","runs":1234,"wickets":150}
+    ]
+
+CSK=[{"jn":18,"p_name":"MS Dhoni","team":"chennai super kings","runs":5432,"wickets":15},
+    {"jn":7,"p_name":"Ravindra Jadeja","team":"chennai super kings","runs":2345,"wickets":120},
+    {"jn":9,"p_name":"Suresh Raina","team":"chennai super kings","runs":5567,"wickets":20},
+    {"jn":3,"p_name":"Dwayne Bravo","team":"chennai super kings","runs":3456,"wickets":150},
+    {"jn":1,"p_name":"Faf du Plessis","team":"chennai super kings","runs":2345,"wickets":10},
+    {"jn":99,"p_name":"Deepak Chahar","team":"chennai super kings","runs":1234,"wickets":100}
+    ]
+
+RCB=[{"jn":18,"p_name":"Virat Kohli","team":"royal challengers bangalore","runs":5432,"wickets":15},
+    {"jn":9,"p_name":"AB de Villiers","team":"royal challengers bangalore","runs":5567,"wickets":20},
+    {"jn":3,"p_name":"Yuzvendra Chahal","team":"royal challengers bangalore","runs":3456,"wickets":150},
+    {"jn":1,"p_name":"Glenn Maxwell","team":"royal challengers bangalore","runs":2345,"wickets":10},
+    {"jn":99,"p_name":"Mohammed Siraj","team":"royal challengers bangalore","runs":1234,"wickets":100},
+    {"jn":45,"p_name":"Harshal Patel","team":"royal challengers bangalore","runs":5643,"wickets":20}
+    ]
+
+SRH=[{"jn":18,"p_name":"David Warner","team":"sunrisers hyderabad","runs":5432,"wickets":15},
+    {"jn":9,"p_name":"Kane Williamson","team":"sunrisers hyderabad","runs":5567,"wickets":20},
+    {"jn":3,"p_name":"Bhuvneshwar Kumar","team":"sunrisers hyderabad","runs":3456,"wickets":150},
+    {"jn":1,"p_name":"Manish Pandey","team":"sunrisers hyderabad","runs":2345,"wickets":10},
+    {"jn":99,"p_name":"Rashid Khan","team":"sunrisers hyderabad","runs":1234,"wickets":100},
+    {"jn":45,"p_name":"Abhishek Sharma","team":"sunrisers hyderabad","runs":5643,"wickets":20}
+    ]
+
+IPL_t20_db={"MI":MI,"CSK":CSK,"RCB":RCB,"SRH":SRH}
--- movies.py
+++ movies.py
@@ -14,6 +14,27 @@ movises_db["Animal"]=cast_Animal
 movises_db["Pushpa1"]=cast_Pushpa1
 movises_db["Pushpa2"]=cast_Pushpa2
 
+for i in movises_db.keys():
+    print(i)
+'''
+Chhaava
+Dhurandhar
+Animal
+Pushpa1
+Pushpa2'''
+
+
+for i in movises_db.values():
+    print(i)
+    
+'''
+['Vicky Kaushal', 'Rashmika Mandanna', 'Akshaye Khanna', 'Ashutosh Rana', 'Divya Dutta']
+['Ranveer Singh', 'Akshaye Khanna', 'R. Madhavan', 'Sanjay Dutt', 'Arjun Rampal', 'Sara Arjun']
+['Ranbir Kapoor', 'Rashmika Mandanna', 'Anil Kapoor', 'Bobby Deol', 'Triptii Dimri']
+['Allu Arjun', 'Rashmika Mandanna', 'Fahadh Faasil']
+['Allu Arjun', 'Rashmika Mandanna', 'Fahadh Faasil']
+'''
+
 count=0
 for k,v in movises_db.items():
     for n in v:
@@ -21,18 +42,9 @@ for k,v in movises_db.items():
             count=count+1
             print(k)
 print(count)
-
 '''
 Chhaava
 Animal
 Pushpa1
 Pushpa2
 4'''
-
-
-
-
-
-
-
-#print(movises_db.items())
