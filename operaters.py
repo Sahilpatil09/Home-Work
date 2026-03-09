fe91a7d84c62387571fc49418904772aa4e9ce10
Sahilpatil09 <sahilpatil3743@gmail.com>
2026-03-09 14:15:46 +0530

    Added operaters.py file

---
+++ operaters.py
@@ -0,0 +1,231 @@
+#Addtion
+a=10
+b=22
+print(a+b)
+"""
+32"""
+
+a=True
+b=True
+print(True+True)
+'''
+2'''
+
+a=[10,20,"sahil"]
+b=[45,25]
+print(a+b)
+'''
+[10, 20, 'sahil', 45, 25]'''
+
+a=(20,50,32)
+b=(45,25)
+print(a+b)
+'''
+(20, 50, 32, 45, 25)'''
+
+# a={1:"jay"}
+# b={1:"vijay"}
+# print(a+b)
+'''
+TypeError: unsupported operand type(s) for +: 'dict' and 'dict' '''
+
+# a={20,50,32}
+# b={45,25}
+# print(a+b)
+'''
+TypeError: unsupported operand type(s) for +: 'set' and 'set' '''
+
+#subtraction
+a=20
+b=10
+print(a-b)
+'''
+10'''
+
+a=True
+b=True
+print(True-True)
+'''
+0'''
+
+# a=[10,20,"sahil"]
+# b=[45,25]
+# print(a-b)
+'''
+TypeError: unsupported operand type(s) for -: 'list' and 'list' '''
+
+# a=(20,50,32)
+# b=(45,25)
+# print(a-b)
+'''
+TypeError: unsupported operand type(s) for -: 'tuple' and 'tuple' '''
+
+# a={1:"jay"}
+# b={1:"vijay"}
+# print(a-b)
+'''
+TypeError: unsupported operand type(s) for -: 'dict' and 'dict' '''
+
+a={10,20,35,15,45}
+b={5,10,15}
+print(a-b)
+'''
+{35, 20, 45}'''
+
+#multiplication
+a=10
+b=22
+print(a*b)
+'''
+220'''
+
+a=True
+b=True
+print(True*True)
+'''
+1'''
+
+# a=[10,20]
+# b=[45,25]
+# print(a*b)
+'''
+TypeError: can't multiply sequence by non-int of type 'list' '''
+
+# a=(20,50,32)
+# b=(45,25)
+# print(a*b)
+'''
+TypeError: can't multiply sequence by non-int of type 'tuple' '''
+
+# a={1:"jay"}
+# b={1:"vijay"}
+# print(a*b)
+'''
+TypeError: unsupported operand type(s) for *: 'dict' and 'dict' '''
+
+# a={10,20,35,15,45}
+# b={5,10,15}
+# print(a*b)
+'''
+TypeError: unsupported operand type(s) for *: 'set' and 'set' '''
+
+print("---"*20)
+#Division
+
+a=10
+b=2
+print(a/b)
+'''
+5.0'''
+
+a=True
+b=True
+print(True/True)
+'''
+1.0'''
+
+# a=[10,20]
+# b=[45,25]
+# print(a/b)
+'''
+TypeError: unsupported operand type(s) for /: 'list' and 'list' '''
+
+# a=(20,50,32)
+# b=(45,25)
+# print(a/b)
+'''
+TypeError: unsupported operand type(s) for /: 'tuple' and 'tuple' '''
+
+# a={1:"jay"}
+# b={1:"vijay"}
+# print(a/b)
+'''
+TypeError: unsupported operand type(s) for /: 'dict' and 'dict' '''
+
+# a={10,20,35,15,45}
+# b={5,10,15}
+# print(a/b)
+'''
+TypeError: unsupported operand type(s) for /: 'set' and 'set' '''
+
+print("---"*20)
+#floor division
+a=11
+b=2
+print(a//b)
+'''
+5'''
+
+a=True
+b=True
+print(True//True)
+'''
+1'''
+
+# a=[10,20]
+# b=[45,25]
+# print(a//b)
+'''
+TypeError: unsupported operand type(s) for //: 'list' and 'list' '''
+
+# a=(20,50,32)
+# b=(45,25)
+# print(a//b)
+'''
+TypeError: unsupported operand type(s) for //: 'tuple' and 'tuple' '''
+
+# a={1:"jay"}
+# b={1:"vijay"}
+# print(a//b)
+'''
+TypeError: unsupported operand type(s) for //: 'dict' and 'dict' '''
+
+# a={10,20,35,15,45}
+# b={5,10,15}
+# print(a//b)
+'''
+TypeError: unsupported operand type(s) for //: 'set' and 'set' '''
+
+print("---"*20)
+#Modulus
+a=33
+b=5
+print(a%b)
+'''
+3'''
+
+a=True
+b=True
+print(True%True)
+'''
+0'''
+
+# a=[10,20]
+# b=[45,25]
+# print(a%b)
+'''
+TypeError: unsupported operand type(s) for %: 'list' and 'list' '''
+
+# a=(20,50,32)
+# b=(45,25)
+# print(a%b)
+'''
+TypeError: unsupported operand type(s) for %: 'tuple' and 'tuple' '''
+
+# a={1:"jay"}
+# b={1:"vijay"}
+# print(a%b)
+'''
+TypeError: unsupported operand type(s) for %: 'dict' and 'dict' '''
+
+# a={10,20,35,15,45}
+# b={5,10,15}
+# print(a%b)
+'''
+TypeError: unsupported operand type(s) for %: 'set' and 'set' '''
+
+a=20
+b=[10,20]
+print(a+b)
+'''
+TypeError: unsupported operand type(s) for +: 'int' and 'list' '''
