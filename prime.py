55887e6b5ec4ee48c2d38505bbb6fbfc153e6ea2
Sahilpatil09 <sahilpatil3743@gmail.com>
2026-03-12 15:22:48 +0530

    Added prime.py file

---
+++ prime.py
@@ -0,0 +1,22 @@
+# #prime number way 1
+# num=int(input("enter the number:"))
+# count=0
+# for i in range(1,num+1):
+#     if num%i==0:
+#         count +=1
+# if count==2:
+#     print(f"{num} is prime")
+# else:
+#     print(f"{num} is not prime")
+
+#prime number way 2
+num=int(input("enter the number:"))
+
+for i in range(2,num):
+    if num%i==0:
+        print("not prime")
+        break
+else:
+    print("prime")
+
+# prime number way 3
