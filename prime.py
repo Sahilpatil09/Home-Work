a9c94743cfded63d36a7408e7c0281894c8f3f47
Sahilpatil09 <sahilpatil3743@gmail.com>
2026-03-12 15:24:37 +0530

    updated

--- prime.py
+++ prime.py
@@ -1,13 +1,13 @@
 # #prime number way 1
-# num=int(input("enter the number:"))
-# count=0
-# for i in range(1,num+1):
-#     if num%i==0:
-#         count +=1
-# if count==2:
-#     print(f"{num} is prime")
-# else:
-#     print(f"{num} is not prime")
+num=int(input("enter the number:"))
+count=0
+for i in range(1,num+1):
+    if num%i==0:
+        count +=1
+if count==2:
+    print(f"{num} is prime")
+else:
+    print(f"{num} is not prime")
 
 #prime number way 2
 num=int(input("enter the number:"))
